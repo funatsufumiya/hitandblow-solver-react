4d6f9e04a3fd64aeeebc58729e470d08e5fbdf3a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2022-08-21 16:06:59 +0900

    WIP: generatePattern

--- src/Solver.ts
+++ src/Solver.ts
@@ -1,3 +1,5 @@
+import unimplemented from 'ts-unimplemented'
+
 enum Color {
   B, R, G, Y, P, W
 }
@@ -11,16 +13,29 @@ enum GameMode {
 export type ColorSetFilter = (colorSet: ColorSet) => boolean
 export type int = number
 
-interface Move {
-  colorset: ColorSet
-  hits: int
-  blow: int
+class Move {
+
+  constructor(public colorSet: ColorSet, public hits: int, public blow: int) {
+  }
+
+  public static parseEach(colorsetStr: string, hits: int, blow: int): Move {
+    return new Move(Solver.stringToColorSet(colorsetStr), hits, blow)
+  }
+
+  public static parse(str: string): Move {
+    let parts = str.split(",")
+    return this.parseEach(parts[0], parseInt(parts[1]), parseInt(parts[2]))
+  }
 }
 
 class GameState {
   public readonly nextPossiblePatterns: ColorSet[];
 
-  constructor(public moves: Move[], private prevPossiblePatterns: ColorSet[]) {
+  constructor(public moves: Move[], private prevPossiblePatterns: ColorSet[], gameMode: GameMode = GameMode.Duplicable) {
+    if (prevPossiblePatterns.length == 0 && moves.length == 1) {
+      this.prevPossiblePatterns = Solver.generatePatterns(gameMode)
+    }
+
     const patternFilter: ColorSetFilter = Solver.generatePatternFilter(moves[moves.length - 1])
     this.nextPossiblePatterns = Solver.filterPattern(this.prevPossiblePatterns, patternFilter)
   }
@@ -46,6 +61,10 @@ class __Solver {
       case Color.P: return '#ff00ff'
       case Color.W: return '#ffffff'
     }
+  }
+
+  public stringToColorSet = (colorSetStr: string): ColorSet => {
+    return colorSetStr.toUpperCase().split('').map(c => (<any>Color)[c])
   }
 
   public getRandomColor = (): Color => {
@@ -97,8 +116,83 @@ class __Solver {
     return patterns
   }
 
+  private getRegExpSourceContent = (regex: RegExp): string => {
+    let s = regex.source
+    if (s.startsWith('^')) {
+      s = s.substring(1)
+    }
+    if (s.endsWith('$')) {
+      s = s.substring(0, s.length - 1)
+    }
+    return s
+  }
+
   private generatePatternFilterRegex = (move: Move): RegExp => {
-    return new RegExp('^.*$')
+    let str = "";
+    if (move.blow == 0) {
+      let s = this.colorSetToString(move.colorSet)
+      if (move.hits == 1) {
+        str = s[0] + "..."
+        str += "|." + s[1] + ".."
+        str += "|.." + s[2] + "."
+        str += "|..." + s[3] + ""
+      } else if (move.hits == 2) {
+        str = s[0] + s[1] + ".."
+        str += "|" + s[0] + "." + s[2] + "."
+        str += "|" + s[0] + ".." + s[3]
+        str += "|." + s[1] + s[2] + "."
+        str += "|." + s[1] + "." + s[3]
+        str += "|.." + s[2] + s[3]
+      } else if (move.hits == 3) {
+        str = s[0] + s[1] + s[2] + "."
+        str += "|" + s[0] + s[1] + "." + s[3]
+        str += "|" + s[0] + "." + s[2] + s[3]
+        str += "|." + s[1] + s[2] + s[3]
+      } else if (move.hits == 4) {
+        str = s
+      } else {
+        return unimplemented()
+      }
+    } else if (move.hits == 0) {
+      let s = this.colorSetToString(move.colorSet)
+      if (move.blow == 1) {
+        let e = ""
+        str = ""
+        e = "[^" + s[3] + "]"
+        str += s[0] + s[1] + s[2] + e
+        str += s[0] + s[2] + s[1] + e
+        str += s[1] + s[2] + s[0] + e
+        str += s[2] + s[1] + s[0] + e
+        str += s[1] + s[0] + s[2] + e
+        str += s[2] + s[0] + s[1] + e
+
+        e = "[^" + s[0] + "]"
+        str += e + s[1] + s[2] + s[3]
+        str += e + s[1] + s[3] + s[2]
+        str += e + s[2] + s[1] + s[3]
+        str += e + s[3] + s[1] + s[2]
+        str += e + s[2] + s[3] + s[1]
+        str += e + s[3] + s[2] + s[1]
+
+        e = "[^" + s[1] + "]"
+        str += s[0] + e + s[2] + s[3]
+        str += s[0] + e + s[3] + s[2]
+        str += s[2] + e + s[0] + s[3]
+        str += s[3] + e + s[0] + s[2]
+        str += s[2] + e + s[3] + s[0]
+        str += s[3] + e + s[2] + s[0]
+
+        e = "[^" + s[2] + "]"
+        str += s[0] + s[1] + e + s[3]
+        str += s[0] + s[3] + e + s[1]
+        str += s[1] + s[0] + e + s[3]
+      } else {
+        return unimplemented()
+      }
+    } else {
+      return unimplemented()
+    }
+    return new RegExp("^" + str + "$")
   }
 
   public generatePatternFilter = (move: Move): ColorSetFilter => {
@@ -121,4 +215,4 @@ class __Solver {
 const Solver = new __Solver()
 
 export default Solver
-export { Color, GameMode }
+export { Color, GameMode, Move }
--- test/mytest.ts
+++ test/mytest.ts
@@ -1,4 +1,4 @@
-import Solver, { Color, GameMode } from "../src/Solver"
+import Solver, { Color, GameMode, Move } from "../src/Solver"
 
 describe("Solver tests", () => {
   let solver = Solver
@@ -29,5 +29,43 @@ describe("Solver tests", () => {
   test("Solver.colorSetToString()", () => {
     expect(solver.colorSetToString([Color.B, Color.R, Color.G, Color.Y])).toBe("BRGY")
     expect(solver.colorSetToString([Color.B, Color.B, Color.W, Color.W])).toBe("BBWW")
+  })
+
+  test("Solver.stringToColorSet()", () => {
+    expect(solver.stringToColorSet("BRGY")).toEqual([Color.B, Color.R, Color.G, Color.Y])
+    expect(solver.stringToColorSet("BBWW")).toEqual([Color.B, Color.B, Color.W, Color.W])
+    expect(solver.stringToColorSet("brgy")).toEqual([Color.B, Color.R, Color.G, Color.Y])
+  })
+
+  test("Move.parse()", () => {
+    let move = Move.parse("BRGY,1,2")
+
+    expect(move.colorSet).toEqual([Color.B, Color.R, Color.G, Color.Y])
+    expect(move.hits).toBe(1)
+    expect(move.blow).toBe(2)
+  })
+
+  test("Solver.generatePatternFilterRegex() 1", () => {
+    const move = Move.parse("BRGY,1,0")
+    const regex = Solver["generatePatternFilterRegex"](move)
+    expect(regex.source).toMatch("^B...|.R..|..G.|...Y$")
+  })
+
+  test("Solver.generatePatternFilterRegex() 2", () => {
+    const move = Move.parse("BRGY,2,0")
+    const regex = Solver["generatePatternFilterRegex"](move)
+    expect(regex.source).toMatch("^BR..|B.G.|B..Y|.RG.|.R.Y|..GY$")
+  })
+
+  test("Solver.generatePatternFilterRegex() 3", () => {
+    const move = Move.parse("BRGY,3,0")
+    const regex = Solver["generatePatternFilterRegex"](move)
+    expect(regex.source).toMatch("^BRG.|BR.Y|B.GY|.RGY$")
+  })
+
+  test("Solver.generatePatternFilterRegex() 4", () => {
+    const move = Move.parse("BRGY,4,0")
+    const regex = Solver["generatePatternFilterRegex"](move)
+    expect(regex.source).toMatch("^BRGY$")
   })
 })
